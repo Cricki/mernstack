3042158fbcbd672dcea378397cecfbee3a929778
Crick <bt1996@gmx.de>
2018-10-18 11:55:57 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const routes = require('./routes');
+
+// configure body parser for AJAX requests
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//routes
+
+app.use(routes);
+//Bootstrap server
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}.`);
+});
